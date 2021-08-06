595bd3691d751ba6863e3d31fd42e9c857307c96
Joshua Dietrich <Jdietrich0603@gmail.com>
2021-08-06 19:58:10 -0400

    handler

--- app/twilio_handler.py
+++ app/twilio_handler.py
@@ -5,6 +5,7 @@ from .models import (Patient, Message)
 
 class RecieveSend:
     def __init__(self):
+
         values = open("secrets.hidden", "r").read().split()
         self.sid = values[0]
         self.token = values[1]
