bbebe3e2110207ef3b346d845cc53fdd7889854b
Joshua Dietrich <Jdietrich0603@gmail.com>
2021-08-06 17:48:05 -0400

    updated respones automation

--- app/models.py
+++ app/models.py
@@ -16,6 +16,8 @@ class Message(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     message = models.CharField(max_length=1000)
     is_patient = models.BooleanField()
+    is_question = models.BooleanField()
+
 
     def __str__(self):
         return f"{self.patient.first_name} {self.message[:20]} {self.is_patient}"
