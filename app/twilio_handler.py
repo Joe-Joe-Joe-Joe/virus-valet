762205aa7d4965d97eda49e5b95b4d5c49f9e840
Joshua Dietrich <Jdietrich0603@gmail.com>
2021-08-07 21:51:56 -0400

    fixing questions

--- app/twilio_handler.py
+++ app/twilio_handler.py
@@ -56,12 +56,12 @@ class RecieveSend:
         self.symptom_questions = {
             'How old are you?' : lambda x, y : isint(x),
             'Have you been experiencing a dry cough?': lambda x, y : is_yes_or_no(x),
-            'Have you been experiencing a sour throat': lambda x, y : is_yes_or_no(x),
+            'Have you been experiencing a sore throat': lambda x, y : is_yes_or_no(x),
             'Have you been experiencing weakness?' : lambda x, y : is_yes_or_no(x),
             'Have you been experiencing a breathing problem?' : lambda x, y : is_yes_or_no(x),
             'Have you been experiencing drowsiness?' : lambda x, y : is_yes_or_no(x),
             'Have you been experiencing a pain in your chest?' : lambda x, y : is_yes_or_no(x),
-            'Have you been experiencing travel history to infected countries?' : lambda x, y : is_yes_or_no(x),
+            'Have you traveled to infected countries recently?' : lambda x, y : is_yes_or_no(x),
             'Do you have diabetes?': lambda x, y : is_yes_or_no(x),
             'Do you have heart disease?' : lambda x, y : is_yes_or_no(x),
             'Do you have lung disease?': lambda x, y : is_yes_or_no(x),
@@ -70,7 +70,7 @@ class RecieveSend:
             'Do you have high blood pressure?': lambda x, y : is_yes_or_no(x),
             'Do you have kidney disease?': lambda x, y : is_yes_or_no(x),
             'Have you recently had a change in appetite?': lambda x, y : is_yes_or_no(x),
-            'Have you had a loss of sense of smell?': lambda x, y : is_yes_or_no(x),
+            'Do you had a loss of sense of smell?': lambda x, y : is_yes_or_no(x),
         }
 
     def full_reset(self):
